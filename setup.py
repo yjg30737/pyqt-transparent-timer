a3b7775790681b5814e0fa882b8f3957747d4280
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-16 08:08:43 +0900

    Replace png with svg

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    package_data={'pyqt_transparent_timer.style': ['button.css'], 'pyqt_transparent_timer.ico': ['play.png', 'pause.png', 'stop.png', 'settings.png']},
+    package_data={'pyqt_transparent_timer.ico': ['play.svg', 'pause.svg', 'stop.svg', 'settings.svg']},
     description='PyQt transparent timer',
     url='https://github.com/yjg30737/pyqt-transparent-timer.git',
     install_requires=[
